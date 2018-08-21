69cc8467732012272c45ff5ccc51adae20709815
Johnathon Henderson <sweetjohn33@gmail.com>
2018-08-21 11:03:03 -0700

    Added better triggers

--- Game_Function/Triggers.py
+++ Game_Function/Triggers.py
@@ -7,14 +7,26 @@ class Trigger(Enum):
 
     Ignition = 0
     OnTurnStart = 1
-    OnBuildingPhaseEnter = 2
+    OnPrepEnter = 2
     OnWeatherChange = 3
     OnCreatureBuy = 4
     OnDevelopmentBuy = 5
     OnMagicBuy = 6
-    OnCardAct = 7
-    Continuous = 8
-    assistfromgrave = 9
+    OnBuildingSell = 7
+    OnMagicSell = 8
+    OnMonsterTribute = 9
+    OnCardActivate = 10
+    OnCardResolve = 11
+    EndPrep = 12
+    OnAttackEnter = 13
+    OnMonsterAttack = 14
+    AfterMonsterAttack = 15
+    EndAttack = 16
+    OnPostEnter = 17
+    EndPost = 18
+    OnCardPlace = 19
+    OnEnterGrave = 20
+
 
 
 
